7f9f1220a900d3d6894875e2bef5eb96ffc2d800
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-08-12 01:48:45 +0530

    Update App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Admin from './Admin';
+import Admin from './admin';
 import Quiz from './Quiz';
 
 function App() {
